4dc78c66297b4c7e21d6fbf4de097e04800c4fde
Neesou <93050748+Neesou@users.noreply.github.com>
2023-09-13 06:04:50 +0200

    Added Images and startet working on the Slideshow

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# PascaldietzDe
+# Angular_First_Page
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.1.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'pascaldietz.de';
+export class AppComponent implements OnInit{
+  images = ['Ceekay.jpg','Coffe.jpg','Light.jpg'];
+  headlines = []
+  currentImage = 0
+  showImage = true
+
+  ngOnInit(){
+    this.updateImage();
+  }
+
+  updateImage(){
+    setInterval(()=>{
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+
+      setTimeout(()=>{
+        this.showImage = true
+      },10)
+
+    },8000)
+  }
+
+
 }
